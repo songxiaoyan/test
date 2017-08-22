c434a2233e80f74a66c56e93554b250155b9bed5
songxiaoyan <sxyynbm@163.com>
2017-08-22 14:13:22 +0800

    做了改动

--- Test/src/com/sxy/test/TestDemo.java
+++ Test/src/com/sxy/test/TestDemo.java
@@ -5,7 +5,7 @@ public class TestDemo {
 	public static void main(String[] args) {
 		System.out.println("My Name Is XXX");
 		//新添加的代码，上传看看结果
-		System.out.println("hello world");
+		System.err.println("hello world");
 	}
 
 }
